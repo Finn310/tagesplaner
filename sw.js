a9381d30c6b92c429cc0df7e2496d55147f841f2
Finn310 <finn05@live.de>
2026-07-15 14:53:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,21 @@
-const CACHE = 'tagesplaner-v5';
-const FILES = [
-  '/index.html',
-  '/manifest.json'
-];
+const CACHE = 'tagesplaner-v6';
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES))
+    caches.open(CACHE).then(c => c.addAll([
+      './',
+      './index.html',
+      './manifest.json',
+      './sw.js',
+      './icons/icon-72.png',
+      './icons/icon-96.png',
+      './icons/icon-128.png',
+      './icons/icon-144.png',
+      './icons/icon-152.png',
+      './icons/icon-192.png',
+      './icons/icon-384.png',
+      './icons/icon-512.png'
+    ]))
   );
   self.skipWaiting();
 });
@@ -22,6 +31,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/index.html')))
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
   );
 });
